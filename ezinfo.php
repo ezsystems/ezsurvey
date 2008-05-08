a2435dfdb6b15257bccfd6624e794cc870629b66
Bjørn Reiten <br@ez.no>
2008-05-08 13:58:34 +0000

    - Updated docs from beta to release 1.
    git-svn-id: http://svn.projects.ez.no/ezsurvey_ezp4/trunk@39 0e881d94-d0d9-4e96-b7b1-7dbe0c2dc8ad

--- ezinfo.php
+++ ezinfo.php
@@ -27,7 +27,7 @@ class ezsurveyInfo
     static function info()
     {
         return array( 'Name' => "eZ Survey",
-                      'Version' => "2.0-beta1",
+                      'Version' => "2.0-1",
                       'Copyright' => "Copyright (C) 1999-2008 eZ Systems AS",
                       'License' => "GNU General Public License v2.0"
                      );
