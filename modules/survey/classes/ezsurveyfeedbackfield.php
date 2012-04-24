a00ae752791cac6a260059a65ee92b68c47503b9
Patrick Allaert <pa@ez.no>
2012-04-24 12:29:38 +0200

    Fixed: copyright header

--- modules/survey/classes/ezsurveyfeedbackfield.php
+++ modules/survey/classes/ezsurveyfeedbackfield.php
@@ -1,31 +1,39 @@
 <?php
 //
-// Definition of eZSurveyFeedbackField class
+// Created on: <29-Jan-2010 16:02:35 br>
 //
-// Created on: Created on: Created on: <29-Jan-2010 16:02:35 br>
+// Copyright (C) 1999-2012 eZ Systems AS. All rights reserved.
 //
-// Copyright (C) 2010 Grenland Web AS. All rights reserved.
+// This source file is part of the eZ Publish (tm) Open Source Content
+// Management System.
 //
 // This file may be distributed and/or modified under the terms of the
 // "GNU General Public License" version 2 as published by the Free
-// Software Foundation and appearing in the file LICENSE included in
+// Software Foundation and appearing in the file LICENSE.GPL included in
 // the packaging of this file.
+//
+// Licencees holding valid "eZ Publish professional licences" may use this
+// file in accordance with the "eZ Publish professional licence" Agreement
+// provided with the Software.
 //
 // This file is provided AS IS with NO WARRANTY OF ANY KIND, INCLUDING
 // THE WARRANTY OF DESIGN, MERCHANTABILITY AND FITNESS FOR A PARTICULAR
 // PURPOSE.
 //
+// The "eZ Publish professional licence" is available at
+// http://ez.no/products/licences/professional/. For pricing of this licence
+// please contact us via e-mail to licence@ez.no. Further contact
+// information is available at http://ez.no/home/contact/.
+//
 // The "GNU General Public License" (GPL) is available at
 // http://www.gnu.org/copyleft/gpl.html.
 //
-// Contact licence@grenlandweb.no if any conditions of this licencing isn't clear to
+// Contact licence@ez.no if any conditions of this licencing isn't clear to
 // you.
 //
 
-/*! \file ezsurveyfeedbackfield.php
-*/
-
 /*!
+  Definition of eZSurveyFeedbackField class
   \class eZSurveyFeedbackField ezsurveyfeedbackfield.php
   \brief The class eZSurveyFeedbackField does
 
