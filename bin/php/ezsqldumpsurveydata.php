0dcd13488d73a16c19174d2097b8b44454f6cdcc
Bertrand Dunogier <bd@ez.no>
2014-05-14 13:54:03 +0200

    EZP-22128: More license changes
    (cherry picked from commit cfebcaf30c9d587e2d4c4f88f138c1fc91dd6603)

--- bin/php/ezsqldumpsurveydata.php
+++ bin/php/ezsqldumpsurveydata.php
@@ -1,46 +1,12 @@
 #!/usr/bin/env php
 <?php
-//
-// Created on: <30-May-2007 16:19:12 bjorn>
-//
-// ## BEGIN COPYRIGHT, LICENSE AND WARRANTY NOTICE ##
-// SOFTWARE NAME: eZ Publish
-// SOFTWARE RELEASE: 4.0.x
-// COPYRIGHT NOTICE: Copyright (C) 1999-2013 eZ Systems AS
-// SOFTWARE LICENSE: GNU General Public License v2.0
-// NOTICE: >
-//   This program is free software; you can redistribute it and/or
-//   modify it under the terms of version 2.0  of the GNU General
-//   Public License as published by the Free Software Foundation.
-//
-//   This program is distributed in the hope that it will be useful,
-//   but WITHOUT ANY WARRANTY; without even the implied warranty of
-//   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-//   GNU General Public License for more details.
-//
-//   You should have received a copy of version 2.0 of the GNU General
-//   Public License along with this program; if not, write to the Free
-//   Software Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston,
-//   MA 02110-1301, USA.
-//
-//
-// ## END COPYRIGHT, LICENSE AND WARRANTY NOTICE ##
-//
-
-/*! \file writesurveyschema.php
-*/
-
-/*!
-  \class Writesurveyschema writesurveyschema.php
-  \brief The class Writesurveyschema does
-
-*/
-
-//include_once( 'lib/ezdbschema/classes/ezdbschema.php' );
-//include_once( 'lib/ezdb/classes/ezdb.php' );
-//include_once( 'lib/ezutils/classes/ezcli.php' );
-
-//include_once( 'kernel/classes/ezscript.php' );
+/**
+ * This file is part of the eZSurvey extension.
+ *
+ * @copyright Copyright (C) eZ Systems AS. All rights reserved.
+ * @license For full copyright and license information view LICENSE file distributed with this source code.
+ * @version //autogentag//
+ */
 
 require 'autoload.php';
 
